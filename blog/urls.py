43a1e76dd7a0c006f30499ed46037b6eb9df5e30
DinuraRajapakshe <user@dinurarajapakshe@gmail.com>
2022-10-14 00:55:32 +0530

    Commit urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -18,10 +18,12 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('about/',views.about, name='blog-about'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-       path('api/posts', post_list)
-]
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/del/', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>/follows', FollowsListView.as_view(), name='user-follows'),
