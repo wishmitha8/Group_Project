8da52d8f3c86bdc7864f8b62c6fc4145aebd5821
DinuraRajapakshe <user@dinurarajapakshe@gmail.com>
2022-10-14 00:33:18 +0530

    commit 3 view.py

--- blog/views.py
+++ blog/views.py
@@ -204,4 +204,71 @@ class FollowersListView(ListView):
         data = super().get_context_data(**kwargs)
         data['follow'] = 'followers'
         return data
-       
+
+# Like Functionality====================================================================================
+
+@login_required
+def postpreference(request, postid, userpreference):
+        
+        if request.method == "POST":
+                eachpost= get_object_or_404(Post, id=postid)
+
+                obj=''
+
+                valueobj=''
+
+                try:
+                        obj= Preference.objects.get(user= request.user, post= eachpost)
+
+                        valueobj= obj.value #value of userpreference
+
+
+                        valueobj= int(valueobj)
+
+                        userpreference= int(userpreference)
+                
+                        if valueobj != userpreference:
+                                obj.delete()
+
+
+                                upref= Preference()
+                                upref.user= request.user
+
+                                upref.post= eachpost
+
+                                upref.value= userpreference
+
+
+                                if userpreference == 1 and valueobj != 1:
+                                        eachpost.likes += 1
+                                        eachpost.dislikes -=1
+                                elif userpreference == 2 and valueobj != 2:
+                                        eachpost.dislikes += 1
+                                        eachpost.likes -= 1
+                                
+
+                                upref.save()
+
+                                eachpost.save()
+                        
+                        
+                                context= {'eachpost': eachpost,
+                                  'postid': postid}
+
+                                return redirect('blog-home')
+
+                        elif valueobj == userpreference:
+                                obj.delete()
+                        
+                                if userpreference == 1:
+                                        eachpost.likes -= 1
+                                elif userpreference == 2:
+                                        eachpost.dislikes -= 1
+
+                                eachpost.save()
+
+                                context= {'eachpost': eachpost,
+                                  'postid': postid}
+
+                                return redirect('blog-home')
+                           
