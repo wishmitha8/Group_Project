23023c63864611387aa7b9de0633107721ed9266
amaranathddrr <rivinduamaranath7@gmail.com>
2022-10-14 11:02:48 +0530

    User

--- users/forms.py
+++ users/forms.py
@@ -9,7 +9,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1']
+        fields = ['userID', 'email', 'password1']
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -17,7 +17,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['userID', 'email']
 
 
 class ProfileUpdateForm(forms.ModelForm):
