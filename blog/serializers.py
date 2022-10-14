3c425b91ab6c12c833fca996655269c0285d5ca5
DinuraRajapakshe <user@dinurarajapakshe@gmail.com>
2022-10-14 11:05:49 +0530

    seri.2

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,4 +7,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']        
         
