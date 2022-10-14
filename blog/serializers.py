b96c88afd0b77ee18e683204e488f90f03b01384
DinuraRajapakshe <user@dinurarajapakshe@gmail.com>
2022-10-14 11:06:51 +0530

    seri4

--- blog/serializers.py
+++ blog/serializers.py
@@ -16,3 +16,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post 
+        fields = ['content', 'author']        
